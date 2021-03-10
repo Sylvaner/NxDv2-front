0158408ba735e2260c4457a38631f17547f0643f
Sylvaner <sylvain.dangin@gmail.com>
2021-03-10 18:19:23 +0100

    Improve device manager

--- src/components/DeviceManager.vue
+++ src/components/DeviceManager.vue
@@ -1,12 +1,39 @@
 <template>
-  <div v-for="category in definedCategories" :key="`cat-${category}`">
+  <div v-for="category in definedCategories" :key="`cat-${category}`" class="devices-category">
     <h2>{{ category }}</h2>
-      <li v-for="device in devicesByCategory(category)" :key="device._id">
-        <span>{{ device.name }}</span>
-      </li>
+    <DataView :value="devicesByCategory(category)" layout="grid">
+      <template #grid="slotProps">
+        <div class="p-col-12 p-md-4">
+          <Card>
+            <template #title>
+              {{ slotProps.data.name }}
+            </template>
+          </Card>
+        </div>
+      </template>
+    </DataView>
   </div>
   <div v-if="unknownDevices.length > 0">
     <h2>Unknown devices</h2>
+    <DataTable :value="unknownDevices" v-model:expandedRows="expandedRows" dataKey="_id">
+      <Column :expander="true" headerStyle="width: 3rem" />
+      <Column field="id" header="id" sortable></Column>      
+      <Column field="name" header="Name" sortable></Column>
+      <Column headerStyle="width: 4rem; text-align: center" bodyStyle="text-align: center; overflow: visible">
+        <template #body="{data}">
+          <Button type="button" icon="pi pi-cog" @click="changeDeviceCategory(data)"></Button>
+        </template>
+      </Column>
+      <template #expansion="slotProps">
+        <div class="expanded-datatable">
+          <h5>Capabilities of {{slotProps.data.name}}</h5>
+          <DataTable :value="capabilitiesToDataTable(slotProps.data.capabilities)">
+            <Column field="name" header="Name" sortable></Column>
+            <Column field="access.get.topic" header="Topic" sortable></Column>
+          </DataTable>
+        </div>
+      </template>
+    </DataTable>
     <ul>
       <li v-for="device in unknownDevices" :key="device._id">
         <span>{{ device.name }}</span>
@@ -18,14 +45,28 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { Device } from '../types';
-import NextDomApi from '../services/NextDomApi';
+import NextDomApi, { Capabilities } from '../services/NextDomApi';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Button from 'primevue/button';
+import DataView from 'primevue/dataview';
+import Card from 'primevue/card';
 
 export default defineComponent({
+  components: {
+    Button,
+    Column,
+    DataTable,
+    DataView,
+    Card
+  },
   data(): {
-    devices: Device[]
+    devices: Device[],
+    expandedRows: Boolean[]
   } {
     return {
-      devices: []
+      devices: [],
+      expandedRows: []
     }
   },
   mounted() {
@@ -45,6 +86,19 @@ export default defineComponent({
       NextDomApi.setCategory(device.id, category).then(() => {
         this.updateDevices();
       });
+    },
+    capabilitiesToDataTable(capabilities: Capabilities) {
+      const result = [];
+      for (let name in capabilities) {
+        result.push({
+          name,
+          access: capabilities[name]
+        });
+      }
+      return result;
+    },
+    changeDeviceCategory(data: any) {
+      console.log(data);
     }
   },
   computed: {
@@ -58,3 +112,13 @@ export default defineComponent({
   }
 })
 </script>
+
+<style>
+.expanded-datatable {
+  padding: 0rem 2rem 1rem 2rem;
+}
+
+.devices-category .p-col-12 {
+  padding: 1rem !important;
+}
+</style>
