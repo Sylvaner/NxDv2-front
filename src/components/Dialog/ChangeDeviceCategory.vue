0ca9438c02530134174bae9b86b729ecdf6af4b3
sylvain <sylvain@MacBook-Air-de-sylvain.local>
2021-03-11 23:42:35 +0100

    Wizard working

--- src/components/Dialog/ChangeDeviceCategory.vue
+++ src/components/Dialog/ChangeDeviceCategory.vue
@@ -1,34 +1,137 @@
 <template>
   <Dialog header="Change device category" v-model:visible="showed">
-    Coucou
+    <template #header>
+    </template>
+    <Listbox v-model="selectedCategory" :options="sortedCategories" optionLabel="name" optionValue="code">
+      <template #option="slotProps">
+        <div class="device-choice">
+          <i :class="slotProps.option.icon + ' fa-fw'"></i>
+          <span>{{slotProps.option.name}}</span>
+        </div>
+      </template>
+    </Listbox>
+    <template #footer>
+		  <Button label="Cancel" icon="pi pi-times" @click="showed = false"/>
+      <Button label="Save" icon="pi pi-check" @click="validChoice" autofocus />
+    </template>
   </Dialog>
 </template>
 
 <script lang="ts">
-import mitt from 'mitt';
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 import Dialog from 'primevue/dialog';
+import { Device } from '../../types';
+import Button from 'primevue/button';
+import Listbox from 'primevue/listbox';
+
+interface CategoryDesc {
+  code?: string;
+  name: string;
+  icon: string;
+  score: number;
+}
 
 export default defineComponent({
   name: 'ChangeDeviceCategory',
   components: {
-    Dialog
+    Dialog,
+    Button,
+    Listbox
   },
-  data: () => {
+  data(): {
+    showed: Boolean,
+    categories: { [code: string]: CategoryDesc }
+    directPoints: { [key: string]: { [name: string]: number } },
+    linkedPoints: { [key: string]: any },
+    selectedCategory: string
+  } {
     return {
-      showed: false
+      showed: false,
+      categories: {
+        light: { name: 'Light', icon: 'fa fa-lightbulb', score: 0},
+        wallPlug: { name: 'Wall Plug', icon: 'fa fa-plug', score: 0},
+        remoteControl: { name: 'Remote control', icon: '', score: 0},
+        doorSensor: { name: 'Door sensor', icon: 'fa fa-door-open', score: 0},
+        motionSensor: { name: 'Motion sensor', icon: 'fa fa-door-open', score: 0},
+        other: { name: 'Others', icon: 'fa fa-door-open', score: 0}
+      },
+      directPoints: {
+        on: {light: 1, wallPlug: 1},
+        brightness: {light: 1},
+        bri: {light: 1},
+        consumption: {wallPlug: 1}
+      },
+      linkedPoints: {
+        ct: {links: ['bri'], target: 'light', score: 10}
+      },
+      selectedCategory: ''
     }
   },
   mounted() {
-    this.eventBus.on('showChangeDeviceCategoryDialog', e => { console.log(e) });
+    this.eventBus.on('showChangeDeviceCategoryDialog', e => { this.showDialog(e); });
   },
   methods: {
-    showDialog(data: any) {
-      console.log(data);
+    showDialog(deviceData: Device) {
+      this.resetScores();
+      this.scoring(deviceData);
+      this.showed = true;
+      this.selectedCategory = this.sortedCategories[0].code!;
+    },
+    resetScores() {
+      for (const scoreCategory in this.categories) {
+        this.categories[scoreCategory].score = 0;
+      }
+    },
+    scoring(deviceData: Device) {
+      const capabilities = Object.keys(deviceData.capabilities);
+      this.searchDirectPointsFromCapabilitiesNames(capabilities);
+      this.searchLinkedPointsFromCapabilitiesNames(capabilities);
+    },
+    searchDirectPointsFromCapabilitiesNames(deviceCapabilities: string[]) {
+      for (const capabilityToTest in this.directPoints) {
+        if (deviceCapabilities.includes(capabilityToTest)) {
+          this.attribDirectPoints(capabilityToTest)
+        }
+      }
+    },
+    attribDirectPoints(foundCapability: string) {
+      for (const target in this.directPoints[foundCapability]) {
+        this.categories[target].score += this.directPoints[foundCapability][target];
+      }
+    },
+    searchLinkedPointsFromCapabilitiesNames(deviceCapabilities: string[]) {
+      for (const capabilityToTest in this.linkedPoints) {
+        if (deviceCapabilities.includes(capabilityToTest)) {
+          this.testLinkPoints(deviceCapabilities, this.linkedPoints[capabilityToTest])
+        }
+      }
+    },
+    testLinkPoints(deviceCapabilities: string[], linkData: {links: string[], target: string, score: number}) {
+      let linksFound = 0;
+      for (const link of linkData.links) {
+        if (deviceCapabilities.includes(link)) {
+          ++linksFound;
+        }
+      }
+      if (linksFound === linkData.links.length) {
+        this.categories[linkData.target].score += linkData.score;
+      }
+    },
+    validChoice() {
+      console.log(this.selectedCategory);
+    }
+  },
+  computed: {
+    sortedCategories(): CategoryDesc[] {
+      const listCategoriesFormat = Object.keys(this.categories).map((code) => { return {code, ...this.categories[code]}});
+      return listCategoriesFormat.sort((a: any, b: any) => (a.score < b.score) ? 1 : -1);
     }
   }
 })
 </script>
 
 <style>
+.device-choice span {
+  margin-left: 1rem;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 const eventBus = mitt();
 
